100ceb69cf35396eda85f3e6d97d9979c6f75fe7
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-10-29 12:28:58 +0100

    added defbasinfo its not finished yet.

---
+++ week1/day4_typescripts/definebasicinfo.ts
@@ -0,0 +1,18 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+let myname: string = 'Gabor Kerekes';
+console.log(myname);
+
+// Your age as an integer
+let myAge = 27;
+console.log(myAge);
+
+// Your height in meters as a float
+let myheight: number = 1.83;
+console.log(myheight);
+
+// Whether you are married or not as a boolean
+let maritalStatus: boolean = false;
+console.log(maritalStatus);
