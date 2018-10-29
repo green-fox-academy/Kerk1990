b15e1456247106e721228235e000ffad238b2a31
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-10-29 11:32:23 +0100

    Added first six exercises.

---
+++ week1/day4_typescript/helloothers.ts
@@ -0,0 +1,7 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines like:
+
+console.log("Hello, David!");
+console.log("Hello, Agi!");
+console.log("Hello, Marci!");
