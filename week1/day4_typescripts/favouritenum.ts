38a08b10e62153f8c593645e4db0a64058b40288
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-10-29 12:00:00 +0100

    Added favourite number and swap.

---
+++ week1/day4_typescripts/favouritenum.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+let favoriteNumber: number = 8
+
+// Store your favorite number in a variable (as a number)
+// And print it like this: 'My favorite number is: 8'
+let answer: string = "My favorite number is: ";
+console.log(answer + favoriteNumber);
