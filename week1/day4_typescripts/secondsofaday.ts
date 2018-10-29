58602fb3f3005e631a4294cef898a0b8d149740e
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-10-29 14:06:05 +0100

    added secondsofaday

---
+++ week1/day4_typescripts/secondsofaday.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
+
+// Write a program that prints the remaining seconds (as an integer) from a
+// day if the current time is represented by these variables
+
+let remainingSecs: number = (24 - currentHours) * 3600 + (60 - currentMinutes) * 60 + (60 - currentSeconds);
+
+console.log("Remaining seconds from the day: " + remainingSecs + " seconds");
