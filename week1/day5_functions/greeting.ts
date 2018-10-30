2564063b89c28a771c6384838980eafe6012db4a
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-10-30 10:57:14 +0100

    added greeting

---
+++ week1/day5_functions/greeting.ts
@@ -0,0 +1,15 @@
+// -  Create variable named `name` and assign the value `Greenfox` to it
+// -  Create a function called `greet` that greets it's input parameter
+//     -  Greeting is printing e.g. `Greetings, dear Greenfox`
+//     -  Prepare for the special case when no parameters are given
+// -  Greet `name
+
+let name1:string = "Greenfox";
+    function greet(Hello:string="Gabor"){
+
+        console.log(greet + Hello);
+
+        
+    }
+
+    greet(name1);
