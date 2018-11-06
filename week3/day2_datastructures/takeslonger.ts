19781f0c53c5869cbef46085b7ecf85742a70f86
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-06 11:52:39 +0100

    almost good

---
+++ week3/day2_datastructures/takeslonger.ts
@@ -0,0 +1,9 @@
+'use strict';
+// When saving this quote a disk error has occured. Please fix it.
+// Add "always takes longer than" to between the words "It" and "you"
+
+let quote: string = 'Hofstadter\'s Law: It you expect, even when you take into account Hofstadter\'s Law.'
+let add: string = "always takes longer than";
+quote = quote.substr(0, 20) + "add" + quote.substr(20);
+
+console.log(quote);
