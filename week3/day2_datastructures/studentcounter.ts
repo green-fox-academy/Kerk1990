f2d70ab8fd725a06ccb7d75ef54d6bfe50c9f5f3
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-06 15:06:19 +0100

    finished, looks good

--- week3/day2_datastructures/studentcounter.ts
+++ week3/day2_datastructures/studentcounter.ts
@@ -1,16 +1,40 @@
 'use strict';
 
 const students: any[] = [
-  {name: 'Theodor', age: 3, candies: 2},
-  {name: 'Paul', age: 9.5, candies: 2},
-  {name: 'Mark', age: 12, candies: 5},
-  {name: 'Peter', age: 7, candies: 3},
-  {name: 'Olaf', age: 12, candies: 7},
-  {name: 'George', age: 10, candies: 1}
+    { name: 'Theodor', age: 3, candies: 2 },
+    { name: 'Paul', age: 9.5, candies: 2 },
+    { name: 'Mark', age: 12, candies: 5 },
+    { name: 'Peter', age: 7, candies: 3 },
+    { name: 'Olaf', age: 12, candies: 7 },
+    { name: 'George', age: 10, candies: 1 }
 ];
 
 // create a function that takes a list of students and logs: 
 // - how many candies are owned by students
+function sumCandies(list: any[]): void {
+    let candyNum: number = 0;
+
+    list.forEach((Element) => {
+        candyNum = candyNum + Element.candies;
+    })
+
+    console.log("The number of candies owned by students: " + candyNum);
+}
+
+sumCandies(students);
 
 // create a function that takes a list of students and logs:
 // - Sum of the age of people who have lass than 5 candies
+
+
+function ageOfStudents(list: any[]): void {
+    let ageSum: number = 0;
+    for (let i: number = 0; i < list.length; i++) {
+        if (list[i].candies < 5) {
+            ageSum = ageSum + list[i].age;
+        }
+    }
+    console.log("Sum of the age of people with less than 5 candies: " + ageSum);
+}
+
+ageOfStudents(students);
