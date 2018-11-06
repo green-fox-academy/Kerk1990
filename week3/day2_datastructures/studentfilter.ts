8cc36abea6354e530e7e77e97b0e8c1b955f85e8
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-06 16:00:37 +0100

    added candies average, finished

--- week3/day2_datastructures/studentfilter.ts
+++ week3/day2_datastructures/studentfilter.ts
@@ -10,11 +10,11 @@ const students: any[] = [
 
 // create a function that takes a list of students and logs:
 // - Who has got more candies than 4 candies
-function candiesMoreThan4(arr: any[]) {
+function candiesMoreThan4(list: any[]) {
     let names: string[] = [];
-    for (let i: number = 0; i < arr.length; i++) {
-        if (arr[i].candies > 2) {
-            names.push(arr[i].name);
+    for (let i: number = 0; i < list.length; i++) {
+        if (list[i].candies > 4) {
+            names.push(list[i].name);
         }
     }
     console.log("These students have more than 4 candies: " + names.join(' and '));
@@ -23,3 +23,14 @@ candiesMoreThan4(students);
 
 // create a function that takes a list of students and logs: 
 //  - how many candies they have on average
+
+function numberOfCandies(list: any[]) {
+    let averageCandies: number = 0;
+    for (let i: number = 0; i < list.length; i++) {
+
+        averageCandies += list[i].candies / list.length;
+    }
+    console.log("The average of the number of candies is : " + averageCandies);
+}
+
+numberOfCandies(students);
