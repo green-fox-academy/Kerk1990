bd2be8ade9fb141c14305ddc9c9a27b81c695b57
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-09 11:01:12 +0100

    feat(envelopestar): just need the middle line

--- week3/day_4_project/envelope_star.ts
+++ week3/day_4_project/envelope_star.ts
@@ -7,26 +7,28 @@ const ctx = canvas.getContext('2d');
 
 for (let i: number = 1; i < 10; i++) {
     ctx.beginPath();
-    ctx.moveTo(i * 30, 300);
-    ctx.lineTo(canvas.width / 2, 300 + i * 30);
-    ctx.strokeStyle = 'red';
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(600 + (-i*30), canvas.width /2);
-    ctx.lineTo(300, 300 +(i*30));
-    ctx.strokeStyle = 'red';
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(300, i * 30);
-    ctx.lineTo(300 + i * 30, canvas.height / 2);
+    ctx.moveTo(i * 30, canvas.height / 2);
+    ctx.lineTo(canvas.width / 2, canvas.height / 2 + i * 30);
     ctx.strokeStyle = '#53D836';
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.moveTo(canvas.height /2, 300+(-i*30));
-    ctx.lineTo(i * 30, 300);
+    ctx.moveTo(canvas.height + (-i * 30), canvas.width / 2);
+    ctx.lineTo(canvas.height / 2, canvas.width / 2 + (i * 30));
+    ctx.strokeStyle = '#53D836';
+    ctx.stroke();
+}
+for (let j: number = 1; j < 10; j++) {
+
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, j * 30);
+    ctx.lineTo(canvas.width / 2 + j * 30, canvas.height / 2);
+    ctx.strokeStyle = '#53D836';
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(canvas.height / 2, canvas.width / 2 + (-j * 30));
+    ctx.lineTo(j * 30, canvas.width / 2);
     ctx.strokeStyle = '#53D836';
     ctx.stroke();
 }
