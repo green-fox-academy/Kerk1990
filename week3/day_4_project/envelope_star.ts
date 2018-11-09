16024d2b5df4934cd46a63b340ab138d6a21d803
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-09 10:52:39 +0100

    feat(envelopestar):now got all four sides

--- week3/day_4_project/envelope_star.ts
+++ week3/day_4_project/envelope_star.ts
@@ -9,12 +9,24 @@ for (let i: number = 1; i < 10; i++) {
     ctx.beginPath();
     ctx.moveTo(i * 30, 300);
     ctx.lineTo(canvas.width / 2, 300 + i * 30);
-    ctx.strokeStyle = '#D73EF2';
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(600 + (-i*30), canvas.width /2);
+    ctx.lineTo(300, 300 +(i*30));
+    ctx.strokeStyle = 'red';
     ctx.stroke();
 
     ctx.beginPath();
     ctx.moveTo(300, i * 30);
     ctx.lineTo(300 + i * 30, canvas.height / 2);
+    ctx.strokeStyle = '#53D836';
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(canvas.height /2, 300+(-i*30));
+    ctx.lineTo(i * 30, 300);
     ctx.strokeStyle = '#53D836';
     ctx.stroke();
 }
