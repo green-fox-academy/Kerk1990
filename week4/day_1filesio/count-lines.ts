7100f82dfa746e9d32c5c6461177d13dd2298388
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-12 13:44:23 +0100

    feat(countlines): working with wrong file

---
+++ week4/day_1filesio/count-lines.ts
@@ -0,0 +1,37 @@
+// Write a function that takes a filename as string,
+// then returns the number of lines the file contains.
+// It should return zero if it can't open the file, and
+// should not raise any error.
+
+export { };
+const fs = require('fs');
+
+function readFromFile(fileName: string): string {
+    try {
+      return fs.readFileSync('my-file.txt', 'utf-8');
+
+    } catch(e) {
+
+      console.log(e.message);
+
+      return null;
+    }
+  }
+  function countLines(fileName: string): number {
+
+    const fileContent: string = readFromFile(fileName);
+
+    let lineCount: number = 0;
+
+    if (fileContent === null) {
+
+      lineCount = 0;
+
+    } else {
+        
+      lineCount = fileContent.split('\n').length;
+    }
+    return lineCount;
+  }
+  
+console.log(countLines('my-file.txt'));
