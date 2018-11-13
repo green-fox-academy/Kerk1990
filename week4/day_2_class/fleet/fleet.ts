1cbdac9a69e16d1e4dc2accbdf36c8b74c9f88b2
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-13 15:17:58 +0100

    added fleet.ts

---
+++ week4/day_2_class/fleet/fleet.ts
@@ -0,0 +1,23 @@
+import { Thing } from "./thing";
+
+class Fleet {
+    private things: Thing[];
+
+    constructor() {
+        this.things = [];
+    }
+
+    add(thing: Thing) {
+        this.things.push(thing);
+    }
+    thingCompleted(i: number) {
+        this.things[i].complete();
+    }
+    print() {
+        for (let j: number = 0; j < this.things.length; j++) {
+            console.log(j + 1 + '' + this.things[j].print());
+        }
+    }
+}
+
+export { Fleet };
