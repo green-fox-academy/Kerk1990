1046391c179262dc0a42c1b2e4b58a1ea7a6633b
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-14 13:46:31 +0100

    added station.ts

--- week4/day_3_class/station/station.ts
+++ week4/day_3_class/station/station.ts
@@ -1,6 +1,15 @@
 import { Car } from "./car"
 export class Station {
+    stationName: string;
+    gasAmount: number;
 
-    
+    constructor(stationName: string, gasAmount: number) {
+        this.stationName = stationName;
+        this.gasAmount = gasAmount;
+    }
+
+    refill(car: Car) {
+        this.gasAmount -= (car.capacity - car.carGasAmount);
+        car.carGasAmount = car.capacity;
+    }
 }
-
