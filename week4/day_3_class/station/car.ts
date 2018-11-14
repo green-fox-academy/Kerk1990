6132503467552af8f695da6d84613ba85639ab02
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-14 13:46:11 +0100

    car.ts is working

--- week4/day_3_class/station/car.ts
+++ week4/day_3_class/station/car.ts
@@ -1,12 +1,23 @@
 import { Station } from "./station"
 export class Car {
 
-    gasAmount: number;
+    carID: string;
+    carGasAmount: number;
     capacity: number;
-  
-    constructor(gasAmount: number = 0, capacity: number = 100) {
-      this.gasAmount = gasAmount;
-      this.capacity = capacity;
+
+    constructor(carID: string, carGasAmount: number, capacity: number) {
+        this.carID = carID;
+        this.carGasAmount = carGasAmount;
+        this.capacity = capacity;
     }
-  }
-    
+}
+let Shell = new Station('Shell', 1000);
+let carID = new Car('Ford', 15, 56);
+
+console.log(`Fuel in station: ${Shell.stationName} before fuelling ${Shell.gasAmount}`);
+console.log(`Fuel in car Tank: ${carID.carID} before fuelling ${carID.carGasAmount}`);
+
+Shell.refill(carID);
+
+console.log(`Fuel in station: ${Shell.stationName} after fuelling ${Shell.gasAmount}`);
+console.log(`Fuel in car Tank: ${carID.carID} after fuelling ${carID.carGasAmount}`);
