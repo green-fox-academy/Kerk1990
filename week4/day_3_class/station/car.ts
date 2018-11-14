534b4d080df24cf3d149cec8ee68da9834d14096
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-14 11:45:44 +0100

    added car.ts

--- week4/day_3_class/station/car.ts
+++ week4/day_3_class/station/car.ts
@@ -1,6 +1,12 @@
 import { Station } from "./station"
 export class Car {
 
+    gasAmount: number;
+    capacity: number;
+  
+    constructor(gasAmount: number = 0, capacity: number = 100) {
+      this.gasAmount = gasAmount;
+      this.capacity = capacity;
+    }
+  }
     
-}
-
