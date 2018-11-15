196bf16c8823cda6b691b7b2275e19c5c9a7a994
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-15 13:50:11 +0100

    string replace is working

---
+++ week4/day_4_recursion/stringrec.ts
@@ -0,0 +1,11 @@
+// Given a string, compute recursively (no loops) a new string where all the
+// lowercase 'x' chars have been changed to 'y' chars.
+
+function charChange(text:string) {
+if (text.indexOf('x') === -1) return text;
+
+return charChange(text.replace('x','y'));
+
+}
+
+console.log(charChange('xcdsnjfvncxivxvvnisdXXX'));
