5150ef95a03546c5fca827831e221c9d18b8c3b7
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-15 11:36:17 +0100

    working fine

---
+++ week4/day_4_recursion/sumdigit.ts
@@ -0,0 +1,10 @@
+// Given a non-negative int n, return the sum of its digits recursively (no loops). 
+// Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
+// divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+function sumDigit(n:number) {
+if (n == 0) return 0;
+
+return n % 10 + sumDigit(Math.floor(n / 10));
+}
+console.log(sumDigit(880099));
