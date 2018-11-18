2600dee6329145fe152477513d5ab4de0d0c86f3
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-18 18:00:31 +0100

    added person

---
+++ week5/practice/person.ts
@@ -0,0 +1,21 @@
+export class Person {
+  name: string;
+  age: number;
+  gender: string;
+
+  constructor(name: string = 'Jane Doe', age: number = 30, gender: string = 'female') {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  Person
+
+  introduce() {
+    console.log(`Hi, I'm ${this.name} a ${this.age} old ${this.gender}.`)
+
+  }
+  getGoal() {
+    console.log(`My goal is: Live for the moment.`);
+  }
+}
