8272a3fb2b6d47a600eda4229e25492210164193
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-18 17:59:42 +0100

    working, needs some cleaning

---
+++ week5/practice/students.ts
@@ -0,0 +1,27 @@
+export class Student {
+  name: string;
+  age: number;
+  gender: string;
+  prevOrg: string;
+  skippedDays: number;
+
+  constructor(name: string = 'Jane Doe', age: number = 30, gender: string = 'female', prevOrg: string = 'The school of life') {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.prevOrg = prevOrg;
+    this.skippedDays = 0;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.name} a ${this.age} old ${this.gender} from ${this.prevOrg} who skipped ${this.skippedDays}`)
+
+  }
+  getGoal() {
+    console.log('My goal is: Be a software developer.');
+
+  }
+  skipDays(numberOfDays: number) {
+    this.skippedDays += numberOfDays;
+  }
+}
