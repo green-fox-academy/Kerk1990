04ee95bb3b9a271ca8d7834c417fe8ea37f2cbb2
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-18 17:58:10 +0100

    breeding and slaughtering is working

--- week4/day_3_class/farm.ts
+++ week4/day_3_class/farm.ts
@@ -1,6 +1,29 @@
-import { Animal } from "../day_2_class/animal"
+import { listenerCount } from "cluster";
 
-export class Farm {
+class Animal {
+    name: string;
+    hunger: number = 50;
+    thirst: number = 50;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    eat(): void {
+        this.hunger--;
+    }
+
+    drink(): void {
+        this.thirst--;
+    }
+
+    play(): void {
+        this.hunger++;
+        this.thirst++;
+    }
+}
+
+class Farm {
     name: string;
     listOfAnimals: Animal[] = [];
     freeSlots: number;
@@ -9,7 +32,7 @@ export class Farm {
         this.freeSlots = freeSlots;
 
     }
-    breed(animal: Animal) {
+    breed(animal: Animal): boolean {
         if (this.freeSlots > 0) {
             this.listOfAnimals.push(animal);
             this.freeSlots--;
@@ -18,15 +41,36 @@ export class Farm {
             return false;
         }
     }
-    slaughter(animal: Animal) {
+    slaughter(): void {
         let leastHunger = this.listOfAnimals[0].hunger;
-        let index: number;
+        let index: number = 0;
         for (let i: number = 0; i < this.listOfAnimals.length; i++) {
             if (this.listOfAnimals[i].hunger < leastHunger) {
                 leastHunger = this.listOfAnimals[i].hunger;
                 index = i;
             }
         }
-
+        this.listOfAnimals.splice(index, 1);
+        this.freeSlots++;
     }
 }
+const farm = new Farm('Little Big Farm', 10);
+
+farm.breed(new Animal('alien'));
+farm.breed(new Animal('T-Rex'));
+farm.breed(new Animal('flea'));
+farm.breed(new Animal('hedgehog'));
+farm.breed(new Animal('zebra'));
+farm.breed(new Animal('megalodon'));
+
+farm.listOfAnimals[1].eat();
+farm.listOfAnimals[2].drink();
+farm.listOfAnimals[3].play();
+console.log('List of animals before slaughter');
+console.log(farm.listOfAnimals);
+
+
+farm.slaughter();
+
+console.log('List of animals after slaughter');
+console.log(farm.listOfAnimals);
