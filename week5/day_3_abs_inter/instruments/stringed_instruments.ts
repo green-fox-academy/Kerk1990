c51da73ecdd596726d05e411f1f199c4aff161a7
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-21 14:06:58 +0100

    added strings for instruments

---
+++ week5/day_3_abs_inter/instruments/stringed_instruments.ts
@@ -0,0 +1,10 @@
+import { Instrument } from './instrument'
+export abstract class StringedInstrument extends Instrument {
+  protected numberOfStrings: number;
+  constructor(name: string, numberOfStrings: number) {
+    super(name);
+    this.numberOfStrings = numberOfStrings;
+  }
+  abstract sound()
+  abstract play()
+}
