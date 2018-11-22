29ffab958287350766bc36ee79025c3d77759507
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-11-22 15:08:57 +0100

    fixed add & maxOfThree

---
+++ week5/day_4_testing/extensions/extension_test.ts
@@ -0,0 +1,59 @@
+'use strict';
+
+let test = require('tape')
+import { add, maxOfThree, median, isVowel, translate } from './extension';
+
+test('add: 2 and 3 is 5', function (t: any): any {
+  t.equal(add(6, 3), 9);
+  t.end();
+});
+
+test('add: 1 and 4 is 5', function (t: any): any {
+  t.equal(add(6, 9), 15);
+  t.end();
+});
+
+test('max of three: first', function (t: any): any {
+  t.equal(maxOfThree(5, 8, 3), 8);
+  t.end();
+});
+
+test('max of three: third', function (t: any): any {
+  t.equal(maxOfThree(3, 66, 5), 66);
+  t.end();
+});
+
+test('median: four', function (t: any): any {
+  t.equal(median([7, 5, 3, 5]), 5);
+  t.end();
+});
+
+test('median: five', function (t: any): any {
+  t.equal(median([1, 2, 3, 4, 5]), 3);
+  t.end();
+});
+
+test('is vowel: a', function (t: any): any {
+  t.ok(isVowel('a'));
+  t.end();
+});
+
+test('is vowel: u', function (t: any): any {
+  t.ok(isVowel('u'));
+  t.end();
+});
+
+test("is vowel: k", function(t: any): any {
+  t.notOk(isVowel("k"));
+  t.end();
+});
+
+test('translate: bemutatkozik', function (t: any): any {
+  t.equal(translate('bemutatkozik'), 'bevemuvutavatkovozivik');
+  t.end();
+});
+
+test('translate: lagopus', function (t: any): any {
+  t.equal(translate('lagopus'), 'lavagovopuvus');
+  t.end();
+});
