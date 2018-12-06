844281f9b651f351d3b7c9d8336750067225ea59
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-12-06 14:42:36 +0100

    feat(createelement1): added lampLighter to js

--- week6/day2_dom/createelem1.js
+++ week6/day2_dom/createelem1.js
@@ -3,3 +3,7 @@ greenFox.textContent = 'The Green Fox';
 
 const ul = document.querySelector('ul');
 ul.appendChild(greenFox);
+
+const lampLighter = document.createElement('li');
+lampLighter.textContent = 'The Lamplighter';
+ul.appendChild(lampLighter);
