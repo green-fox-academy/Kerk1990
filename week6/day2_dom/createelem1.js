b6f83adf0db8521188c9167789442c0ea9397291
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-12-06 16:06:28 +0100

    feat(createelem1): finished js

--- week6/day2_dom/createelem1.js
+++ week6/day2_dom/createelem1.js
@@ -7,3 +7,15 @@ ul.appendChild(greenFox);
 const lampLighter = document.createElement('li');
 lampLighter.textContent = 'The Lamplighter';
 ul.appendChild(lampLighter);
+
+const header = document.createElement('h1');
+header.textContent = 'I can add elements to the DOM!';
+
+const container = document.querySelector('.container');
+container.appendChild(header);
+
+const image = document.createElement('IMG');
+image.setAttribute("src", 'https://i.pinimg.com/originals/5f/5d/83/5f5d83919d9e6117c182003f70ed6bbd.jpg');
+image.setAttribute('height','400');
+image.setAttribute('width','250');
+container.appendChild(image);
