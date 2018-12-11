b840fc35c9ec7ee1a0531df637b6495e393a1c77
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-12-11 15:49:46 +0100

    feat(giphy): added working js file

--- week8/day1_xhr_callback/main.js
+++ week8/day1_xhr_callback/main.js
@@ -1,5 +1,5 @@
 const xhr = new XMLHttpRequest();
-const API_BASE_URL = "http://api.giphy.com/v1/gifs/search?q=funny+shit&api_key=2JARj1j15MHluVqfnamDwQVvrAd2p3Vz&limit=16";
+const API_BASE_URL = "http://api.giphy.com/v1/gifs/search?q=funny+fail&api_key=2JARj1j15MHluVqfnamDwQVvrAd2p3Vz&limit=16";
 
 xhr.open('GET', `${API_BASE_URL}`, true);
 xhr.send();
@@ -8,6 +8,17 @@ xhr.onreadystatechange = () => {
     if (xhr.status === 200) {
       const response = JSON.parse(xhr.responseText);
       console.log(response);
+      for (let i = 0; i < response.data.length; i++) {
+        let giphyStillURL = response.data[i].images.original_still.url;
+        let gifMove = response.data[i].images.original.url;
+        let gifList = document.querySelector('.gifs');
+        let gif = document.createElement('img');
+        gifList.appendChild(gif);
+        gif.setAttribute('src', giphyStillURL);
+        gif.onclick = () => {
+          gif.setAttribute('src', gifMove);
+        }
+      }
     }
   }
 }
