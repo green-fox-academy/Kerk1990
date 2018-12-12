cb802563f16ea29223368da362a644807fa86d31
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-12-12 14:47:49 +0100

    feat(bubbling):added eventlisteners for directions and zoom

--- week8/day2_eventlistener/bubbling/bubbling.js
+++ week8/day2_eventlistener/bubbling/bubbling.js
@@ -4,3 +4,29 @@ let posX = 0;
 let posY = 0;
 let zoomSize = 200;
 
+buttons.addEventListener('click', e => {
+  if (e.target.dataset.direction === "in") {
+    zoomSize += 10;
+    image.style.backgroundSize = `${zoomSize}%`;
+  }
+  if (e.target.dataset.direction === "out" && zoomSize > 100) {
+    zoomSize -= 10;
+    image.style.backgroundSize = `${zoomSize}%`;
+  }
+  if (e.target.dataset.direction === "left" && posX > 0) {
+    posX -= 10;
+    image.style.backgroundPosition = `${posX}% ${posY}%`;
+  }
+  if (e.target.dataset.direction === "right" && posX < 100) {
+    posX += 10;
+    image.style.backgroundPosition = `${posX}% ${posY}%`;
+  }
+  if (e.target.dataset.direction === "up" && posY > 0) {
+    posY -= 10;
+    image.style.backgroundPosition = `${posX}% ${posY}%`;
+  }
+  if (e.target.dataset.direction === "down" && posY < 100) {
+    posY += 10;
+    image.style.backgroundPosition = `${posX}% ${posY}%`;
+  }
+});
