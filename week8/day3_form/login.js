aff0ac0bcf935695285cfc70fb840c5ce1b6f73c
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-12-13 14:04:29 +0100

    feat(login): not in use yet

---
+++ week8/day3_form/login.js
@@ -0,0 +1,5 @@
+
+// $('password + .glyphicon').on('click', function () {
+//   $(this).toggleClass('glyphicon-eye-close').toggleClass('glyphicon-eye-open');
+//   $('#password').togglePassword();
+// });
