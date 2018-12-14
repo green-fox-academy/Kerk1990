67bdc77e4c17f4c66fcf81fd6b29525f79cb38b1
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-12-14 14:39:52 +0100

    feat(candyshop): modified it to pub

--- week8/day2_eventlistener/candyshop/game.js
+++ week8/day2_eventlistener/candyshop/game.js
@@ -1,22 +1,58 @@
+const createBeers = document.querySelector('.create-beer');
+const numberOfBeers = document.querySelector('.beers');
+const beerCreateSpeed = document.querySelector('.speed');
+const getCocktails = document.querySelector('.buy-cocktails');
+const getWine = document.querySelector('.buy-wines')
+const cocktailCount = document.querySelector('.cocktails');
+const wineCount = document.querySelector('.wines')
+const destroyed = document.querySelector('.get-wasted');
+const image = document.getElementById('.image');
+
 let beers = 0;
 let cocktails = '';
+let wines = '';
 let cocktailCounter = 0;
-let beersPerSec = 0;
+let wineCounter = 0;
+let beerPerSec = 0;
 
-const beersCount = document.querySelector('.beers');
-const cocktails = document.querySelector('.cocktail');
-const beerSpeed = document.querySelector('.speed');
-const createBeer = document.querySelector('.create-beer');
-const buyCocktails = document.querySelector('.buy-cocktails');
-const slotMachine = document.querySelector('.slot-machine');
 
 const addBeers = () => {
   beers++;
-  beerCount.textContent = beers;
+  numberOfBeers.textContent = beers;
 }
 
-const cocktailsPerSec = () => {
-  beers += beersPerSec;
-  beersCount.textContent = beers;
+const beersByTime = () => {
+  beers += beerPerSec;
+  numberOfBeers.textContent = beers;
 }
 
+const wasted = () => {
+  beerPerSec = beerPerSec + 1;
+  beerCreateSpeed.textContent = beerPerSec;
+}
+
+createBeers.addEventListener('click', addBeers);
+
+getCocktails.addEventListener('click', () => {
+  if (beers >= 5) {
+    cocktails += '🍸';
+    cocktailCounter++;
+    cocktailCount.textContent = cocktails;
+    beers -= 5;
+    numberOfBeers.textContent = beers;
+  }
+})
+
+getWine.addEventListener('click', () => {
+  if (beers >= 10) {
+    wines += '🍷';
+    wineCounter++;
+    wineCount.textContent = wines;
+    beers -= 10;
+    numberOfBeers.textContent = beers;
+  }
+})
+
+destroyed.addEventListener('click', wasted);
+
+setInterval(beersByTime, 100);
