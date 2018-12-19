02832c48a8bfb9e3dfb077b668dab865e5a5a498
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-12-19 12:18:07 +0100

    feat(doublinf):added doubling endpoint

--- week9/day3_restBack/frontend/app.js
+++ week9/day3_restBack/frontend/app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 const path = require('path');
-
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/doubling', (req, res) => {
+  const { input } = req.query;
+  if (input) {
+    res.json({
+      received: input,
+      result: input * 2
+    });
+  } else {
+    res.json({
+      error: 'Please provide an input!',
+    });
+  }
+});
 app.use('/assets', express.static('assets'));
 
 app.listen(PORT, () => {
