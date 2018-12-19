460acdee7244c5d89a5e01608693f636b6c77b30
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-12-19 16:41:11 +0100

    feat(arrayhandler): added working app.js file

---
+++ week9/day3_restBack/arrayhandler/app.js
@@ -0,0 +1,41 @@
+'use strict';
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+app.use(express.json());
+
+
+app.post('/array', (req, res) => {
+  const what = req.body.what;
+  const numbers = req.body.numbers;
+  if (what == 'sum') {
+    res.json({
+      result: sum(numbers)
+    });
+  } else if (what == 'multiply') {
+    res.json({
+      result: multiply(numbers)
+    });
+  } else if (what == 'double') {
+    res.json({
+      result: double(numbers)
+    });
+  } else res.json({ error: 'Please provide what to do with the numbers!' })
+});
+
+const sum = (array) => {
+  return array.reduce((a, b) => a + b);
+}
+
+const multiply = (array) => {
+  return array.reduce((a, b) => a * b);
+}
+
+const double = (array) => {
+  return array.map(a => a* 2);
+}
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
