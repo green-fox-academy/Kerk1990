34489c972ed3b1b65ccf0e8f6ed164f3774e0725
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-12-19 15:04:18 +0100

    feat(dountil): added dountil endpoint

--- week9/day3_restBack/frontend/app.js
+++ week9/day3_restBack/frontend/app.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = 8080;
 const path = require('path');
 
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -42,6 +45,41 @@ app.get('/appenda/:appendTo', (req, res) => {
     });
   }
 });
+sumUntil = (inputNum) => {
+  if (inputNum > 0) {
+    return inputNum + sumUntil(inputNum - 1);
+  } else {
+    return 0;
+  }
+}
+
+multiplyUntil = (inputNum) => {
+  if (inputNum > 1) {
+    return inputNum * multiplyUntil(inputNum - 1);
+  } else {
+    return 1;
+  }
+}
+app.post('/dountil/:action',jsonParser,(req, res) => {
+  let sumResult = sumUntil(req.body.until);
+  let multiResult = multiplyUntil(req.body.until);
+  if (req.params.action === 'sum') {
+    res.json({
+      result: sumResult
+    })
+  } else if (req.params.action === 'factor') {
+    res.json({
+      result: multiResult
+    })
+  } else {
+    res.json({
+      error: 'Please provide a valid action'
+    })
+  }
+})
+
+
+
 
 app.use('/assets', express.static('assets'));
 
