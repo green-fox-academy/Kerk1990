75f2c6c43c0637c58a0505eb639a1ba86cf74c10
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-12-19 11:19:03 +0100

    feat(frontend): added js file

---
+++ week9/day3_restBack/frontend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const path = require('path');
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use('/assets', express.static('assets'));
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
