fecba2bc40a72a9814bd534ad14ca6afa85d9c7e
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2018-12-20 16:56:05 +0100

    feat(book): half finished

--- week9/day4_nodesql/books/app.js
+++ week9/day4_nodesql/books/app.js
@@ -6,12 +6,20 @@ const mysql = require('mysql');
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
+app.use(express.json());
 
 const conn = mysql.createConnection({
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD
+});
+
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 conn.connect(err => {
@@ -27,17 +35,17 @@ FROM book_mast, author, publisher, category
 WHERE book_mast.aut_id = author.aut_id 
 AND book_mast.pub_id = publisher.pub_id 
 AND book_mast.cate_id = category.cate_id`
-app.get('/', (req, res) => {
+
+app.get('/bookinfo', (req, res) => {
   conn.query(baseQuery, (err, rows) => {
     if (err) {
       console.log(err.toString());
-      res.status(500).send('Database error');
+      res.status(500).json('Database error');
       return;
     }
-    res.send(rows);
+    res.json(rows);
   });
 });
-
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 });
