d9398682c7b9c209d857f4ab3407f84e93c52b7a
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2019-01-08 12:03:43 +0100

    feat(test):added 2 tests

---
+++ week10/iamgroot/test/test-routes.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const test = require('tape');
+const request = require('supertest');
+const app = require('../routes');
+
+test('groot endpoint', (t) => {
+  request(app)
+    .get('/groot')
+    .expect(210)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) throw err;
+      t.same(res.body, {
+        error: 'I am Groot!'
+      }, ' whitout query test');
+      t.end();
+    });
+});
+
+test('groot endpoint', (t) => {
+  request(app)
+    .get('/groot?somemessage=fdsfdsgfsd')
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end((err, res) => {
+      if (err) throw err;
+      t.same(res.body, {
+        "recieved": 'fdsfdsgfsd',
+        "translated": "I am Groot!"
+      }, ' whitout query test');
+      t.end();
+    });
+});
+
