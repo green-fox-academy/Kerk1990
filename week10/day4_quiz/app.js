22f48d799c76ac4df3cce6c2508824fcb3fcadb1
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2019-01-09 16:41:27 +0100

    feat(quizapp):added query for random numbers

--- week10/day4_quiz/app.js
+++ week10/day4_quiz/app.js
@@ -46,11 +46,34 @@ app.get('/game', (req, res) => {
     });
     console.log(questionId);
     randomNumber = Math.floor(Math.random() * questionId.length);
-  })
-})
 
+    const sqlRandom = `SELECT questions.id, questions.question, answers.id, answers.question_id, answers.is_correct, answers.answer 
+    FROM questions, answers WHERE questions.id = answers.question_id
+    AND questions.id = ${questionId[randomNumber]};`;
 
-
-
+    conn.query(sqlRandom, (err, data) => {
+      if (err) {
+        console.log(err.message);
+        res.status(400).json({
+          error: 'Internal server error'
+        });
+        return;
+      }
+      let tempAnswers = data.map(element => {
+        return {
+          id: element.id,
+          answer: element.answer,
+          is_correct: element.is_correct,
+          question_id: element.question_id
+        };
+      })
+      res.json({
+        id: data[0].id,
+        question: data[0].question,
+        answer: tempAnswers
+      });
+    });
+  });
+});
 
 app.listen(PORT, console.log(`Listening on PORT: ${PORT}`));
