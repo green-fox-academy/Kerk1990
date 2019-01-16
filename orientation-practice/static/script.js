facd5e6888c67f80b3c2d3da685bbb71ed035501
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2019-01-16 09:45:28 +0100

    feat(practice):addded full frontend file

---
+++ orientation-practice/static/script.js
@@ -0,0 +1,46 @@
+'use strict'
+const form = document.querySelector('form');
+const display = document.querySelector('.responseContainer');
+const radioBtns = document.querySelectorAll('form input[type=radio');
+
+
+form.onsubmit = () => {
+  event.preventDefault();
+  const {name, address, base, toppings } = form.elements;
+
+const postReq = new XMLHttpRequest();
+postReq.open('POST','api/orders');
+postReq.setRequestHeader('Content-Type','application/json');
+postReq.send(JSON.stringify({
+  name:name.value,
+  address:address.value,
+  base:base.value,
+  topping: toppings.value,
+}));
+postReq.onload = () => {
+  cnost response = JSON.parse(postReq.responseText);
+  render(response);
+  name.value = null;
+  address.value = null;
+  toppings.value = null;
+  radioUncheck(radioBtns);
+};
+};
+
+const render = (data) => {
+  const ul = document.createElement('ul');
+  const liID = document.createElement('li');
+
+  liID.innerText = `Your order id is: ${data.id}`;
+
+  ul.appendChild(ilID);
+  display.appendChild(ul);
+};
+
+const radioUncheck = (radios) => {
+  radios.fordEach(e => {
+    if (e.checked) {
+      e.checked = false;
+    }
+  });
+};
