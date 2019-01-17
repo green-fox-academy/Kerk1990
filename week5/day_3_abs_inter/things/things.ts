0c75a294d4d00ee13d18008b5196d11596d78b6c
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2019-01-17 10:56:59 +0100

    feat(abs_inter):added ts files

---
+++ week5/day_3_abs_inter/things/things.ts
@@ -0,0 +1,23 @@
+import { Comparable } from './comparable'
+class Thing {
+  private name: string;
+  private completed: boolean;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  public complete() {
+    this.completed = true;
+  }
+  public print() {
+    if (this.completed === true) {
+      return ('[x] ' + this.name);
+    } else {
+      return ('[ ] ' + this.name);
+    }
+
+  }
+}
+
+export { Thing };
