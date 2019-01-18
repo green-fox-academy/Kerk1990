0424c990522a37ec6db0c3992e6dd146c8ed30d7
Gabor Kerekes <43649284+Kerk1990@users.noreply.github.com>
2019-01-18 17:48:36 +0100

    feat(weatherapp):added cities and forecast

--- Angular-weatherApp/weatherApp-pre/src/app/app.module.ts
+++ Angular-weatherApp/weatherApp-pre/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CitiesComponent } from './cities/cities.component';
+import { ForecastComponent } from './forecast/forecast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CitiesComponent,
+    ForecastComponent
   ],
   imports: [
     BrowserModule
